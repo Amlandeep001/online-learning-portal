56563cdf511e0afe66012dc6cd3e6ebd6848bf09
Amlan Deep Nandi <amlandeep.nandi@gmail.com>
2025-08-29 20:45:07 +0530

    Spring Boot version update
    Spring Boot version update

--- course-service/src/main/java/com/example/learning/portal/dto/Course.java
+++ course-service/src/main/java/com/example/learning/portal/dto/Course.java
@@ -1,23 +1,26 @@
 package com.example.learning.portal.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Course {
-    private Integer id;
-    private String name;
-    private String instructor;
-    private List<Rating> ratings = new ArrayList<>();
+public class Course
+{
+	private Integer id;
+	private String name;
+	private String instructor;
+	private List<Rating> ratings = new ArrayList<>();
 
-    public double getAverageRating() {
-        if (ratings.isEmpty()) return 0;
-        return ratings.stream().mapToInt(Rating::getStars).average().orElse(0);
-    }
+	public double getAverageRating()
+	{
+		if(ratings.isEmpty())
+			return 0;
+		return ratings.stream().mapToInt(Rating::getStars).average().orElse(0);
+	}
 }
